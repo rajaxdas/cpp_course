434491078e25a075ffa5c42256d10afe3b028058
Raja <rajaaax7@gmail.com>
2026-09-09 18:43:00 +0530

    Adding c++ files

---
+++ data_types.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    //integer data type
+    /*int a = 4;
+    int b = 5;
+    cout<<"The value of a is: "<<a<<" and the value of b is: "<<b;*/
+
+
+    //float data type
+    // float pi=3.14;
+    // cout<<"this is the value of pi:"<<pi;
+
+
+    // character data type
+    // char c = 'A';
+    // cout<<"The value of c is "<<c;
+
+    //boolean data type
+    bool a = true;
+    cout<<"This is the value of a"<<a;
+    return 0; 
+}
---
+++ i/o_stream.cpp
@@ -0,0 +1,59 @@
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+//     int num1, num2; 
+//     // (<<) this is called insertion operator
+//     cout<<"Enter the value of num1 \n";
+//     //(>>) this is called extraction operator
+//     cin>>num1;
+
+//     cout<<"Enter the value of num2 \n";
+//     cin>>num2;
+    
+//     cout<<"The sum is : "<< num1 + num2;
+//     return 0;
+// }
+
+
+
+#include <iostream>
+#include <iomanip>
+using namespace std;
+//assigning values to variables (updating the value of variables)
+int main(){
+// //     int a = 34;
+// //     cout<<"the value of a was : "<<a;
+// //     a = 45;
+// //     cout<<"the value of a is: "<<a;
+// //     return 0;
+
+
+
+// // if we use constant keyword then we cannot update the value of variable...
+//     const int a = 3;
+//     cout<<"the value of a is : "<<a;
+//     a = 50;
+//     cout<<"the value of a is: "<<a<<endl;
+
+
+
+//maipulators in c++
+int a = 3, b = 78, c = 1234;
+//setw is used to set the width of the output
+// cout<<"the value of a without setw is : "<<a<<endl;
+// cout<<"the value of b without setw is : "<<b<<endl;
+// cout<<"the value of c without setw is : "<<c<<endl;
+
+// cout<<"the value of a is : "<<setw(4)<<a<<endl;
+// cout<<"the value of b is : "<<setw(4)<<b<<endl;
+// cout<<"the value of c is : "<<setw(4)<<c<<endl;
+
+//operator precedence in c++
+// int a = 3, b=4;
+// int c = a*5 + b;
+// cout<<c;
+
+
+
+}
